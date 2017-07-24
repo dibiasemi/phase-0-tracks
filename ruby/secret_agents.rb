db0d7394ef03c3327a2a656b299fdd0baf6c2010
dibiasemi <dibiase.111@gmail.com>
2017-07-23 22:39:17 -0500

    Updated secret_agents.rb with final changes from REPL

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,64 @@
+# ENCRYPT METHOD EXPLAINED
+#input: string of varying length
+#output: word where each index is advanced forward by one position/value
+#process: .next method returns the next value for each index or iteration of the loop
+
+def encrypt(password)
+  next_string = "" #adds space to beginning of message
+  i = 0
+  while i < password.length
+      if password[i] == "z"
+        next_string << "a"
+      elsif password[i] == " "
+        next_string << " "
+      else
+        next_string << password[i].next
+      end
+      i += 1
+    end
+  next_string
+end
+
+# DECRYPT METHOD EXPLAINED
+#input: string of varying length
+#output: word where each index is decremented by one position/value
+#process: index each letter of the alphabet using an "alpha" string and the .index method. The input of the .index method will be the output of the "message[i] where i represents an iteration in the loop. The output of the .index method will be a number. By subtracting one from that "number" and inputing it into the expression alpha[number-1] the output will be a "new_letter" at that index. We can then shovel that "new_letter" onto our initial "next_string" variable and keep building the string with each iteration through the loop.
+
+def decrypt(password)
+  next_string = "" #This is our beginning string
+  i = 0
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  while i < password.length
+    if password[i] == " "
+      next_string << " "
+    else
+      number = alpha.index(password[i])
+      new_letter = alpha[number-1]
+      next_string << new_letter
+    end
+   i += 1
+  end
+  next_string
+end
+
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
+# puts encrypt("helloza goodbye")
+# puts decrypt(encrypt("helloza goodbye"))
+# puts decrypt(encrypt("swordfish"))
+# # The nested methods work because the output of one is equal to the input of another = same word.
+
+# Asks a secret agent (the user) whether they would like to decrypt or encrypt a password
+puts "Would you like to decrypt or encrypt a password?"
+method = gets.chomp
+# Asks them for the password
+puts "Ok then, give me a password to #{method}:"
+password = gets.chomp
+# Conducts the requested operation, prints the result to the screen, and exits
+if method == "encrypt"
+  puts encrypt(password)
+else
+  puts decrypt(password)
+end
