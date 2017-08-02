90c58a6eb99c3bd565f5b39153e61f3084930714
dibiasemi <dibiase.111@gmail.com>
2017-08-02 15:39:53 -0500

    Updated characteristics and personality traits of a puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Puppy Class
+#Characteristics
+# => Size:
+# => Fur Color:
+# => Fur Length:
+# => Fur Style:
+# => Eye color:
+
+# Personality Traits:
+#   Playfulness
+#   Curiosity/Fearlessness
+#   Sociability
+#   Aggressiveness
+#   Shyness/Boldness
