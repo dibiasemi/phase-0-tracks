9e150d1cf8cbef78c7c5a90cf5c2a8ee368e9c4f
dibiasemi <dibiase.111@gmail.com>
2017-08-07 11:26:33 -0500

    Created directory 'game' to house guessing game files

---
+++ ruby/game/Guessing_game.rb
@@ -0,0 +1,67 @@
+
+words = ["couch",
+"alarm",
+"bushes",
+"license",
+"mountain",
+"wiggly",
+"shame",
+"oil",
+"depressed",
+"sticks",
+"lively",
+"sunrise",
+"clam",
+"paper",
+"zonked",
+"useless",
+"dance",
+"bucket",
+"magical",
+"idiotic"]
+
+word = words[rand(words.length)]
+total_chances = word.length
+wrong_guess = 0
+correct_guess = ''
+
+def add_placeholder(rand_word, guessed_letter)
+  placeholder = ''
+  rand_word.chars do |letter|
+    placeholder += (guessed_letter.include? letter)? letter : ' _'
+  end
+  placeholder
+end
+
+puts 'Can you guess what word this is?!'+ add_placeholder(word, "")
+
+while true
+  print "Please guess a letter [#{total_chances - wrong_guess} chances left]:"
+
+  letter = gets.chomp
+
+  if word.include? letter
+
+   if
+      correct_guess = correct_guess + letter
+      placeholder = add_placeholder(word, correct_guess)
+      puts 'Great job! You guessed one of the letters ' + placeholder
+    end
+
+    unless placeholder.include? '_'
+      puts "You guessed it! You're a rockstar"
+      break
+    end
+
+  else
+    puts "Ooops! there is no '#{letter}' in the word "
+    wrong_guess += 1
+
+    if (wrong_guess == total_chances)
+      puts "Sorry kiddo! You're out of guesses!"
+      break
+    else
+      puts 'Keep Guessing' + add_placeholder(word, correct_guess)
+    end
+  end
+end
